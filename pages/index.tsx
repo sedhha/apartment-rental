8aac4331f3029fb7c1d2dbc0d614fb6ae70d4300
sedhha <activity.schoolsh2@gmail.com>
2022-05-22 13:47:28 +0530

    Added Font Config

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,20 +16,21 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-4xl font-bold font-std">
           EPAM Hiring Challenge Apartment Rental
         </h1>
+        <br />
         <button
           type="button"
           onClick={navigateToLogin}
-          className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="mr-2 mb-2 w-1/5 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Login
         </button>
         <button
           type="button"
           onClick={navigateToRegister}
-          className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="mr-2 mb-2 w-1/5 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Register
         </button>
